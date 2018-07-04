cff47d82cddba4baeb3b2132561fb3f60e51bad4
Siva Kumar Gopi <sivakumar1592@gmail.com>
2018-07-04 15:21:01 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBJ2JsZAb2fo0m0HrDVrLa8oJcepcb2pBw',
+  authDomain: 'chatapp-da381.firebaseapp.com',
+  databaseURL: 'https://chatapp-da381.firebaseio.com',
+  projectId: 'chatapp-da381',
+  storageBucket: 'chatapp-da381.appspot.com',
+  messagingSenderId: '566943539642'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
