3403e20e463ac1d8c79eb74cff7a2c1c7170139a
SivagKumar <sivakumar1592@gmail.com>
2018-07-04 18:38:11 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,12 @@ import {AngularFireDatabaseModule} from 'angularfire2/database-deprecated';
 import {AngularFireDatabase} from 'angularfire2/database-deprecated';
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBJ2JsZAb2fo0m0HrDVrLa8oJcepcb2pBw',
-  authDomain: 'chatapp-da381.firebaseapp.com',
+  apiKey: '',
+  authDomain: '',
   databaseURL: 'https://chatapp-da381.firebaseio.com',
   projectId: 'chatapp-da381',
   storageBucket: 'chatapp-da381.appspot.com',
-  messagingSenderId: '566943539642'
+  messagingSenderId: ''
 };
 
 @NgModule({
